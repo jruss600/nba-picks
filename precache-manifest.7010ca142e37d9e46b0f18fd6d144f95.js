10b2c21768bbdff72d79ff04fbc8571b7406fcd7
John Russell <jruss600@gmail.com>
2019-11-02 16:31:42 -0600

    Updates

---
+++ precache-manifest.7010ca142e37d9e46b0f18fd6d144f95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cea17b95e5ba7640405739e9c0fd66a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3589f5bdfd440bcf83b8",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "9404626d9d83ad0b3770",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3589f5bdfd440bcf83b8",
+    "url": "/static/js/2.4cf79bd6.chunk.js"
+  },
+  {
+    "revision": "9404626d9d83ad0b3770",
+    "url": "/static/js/main.88395b73.chunk.js"
+  },
+  {
+    "revision": "6de8e69404aee3674f5a",
+    "url": "/static/js/runtime-main.bff7fbb9.js"
+  }
+]);
